1cf43cccb6510b4d6466f40ce7fb85b1f64141e6
zxeni1 <zacxfitness@gmail.com>
2024-10-29 19:39:45 +1000

    Added list to JS dom

--- src/scripts/dataList.js
+++ src/scripts/dataList.js
@@ -12,10 +12,29 @@ let dataArray = [
     "Clowns"
 ];
 
-dataArray.forEach((costume) => {
-    console.log(costume);
-});
-
 let costumeContainer = document.getElementById("costumeContainer");
 
-costumeContainer.innerText = "Hello World";
+// Create an unordered list tag
+let costumeContainerList = document.createElement("ul");
+
+
+// costumeContainer.innerText = "Hello World";
+
+dataArray.forEach((costume) => {
+    // Verify what we are working with
+    console.log(costume);
+
+    // Create the list element
+    let newCostumeEntry = document.createElement("li");
+
+    // Add the value to html element 
+    newCostumeEntry.innerText = costume;
+
+    
+
+    // Add the element to the container
+    costumeContainerList.appendChild(newCostumeEntry);
+});
+
+// Add the container to the HTML page
+costumeContainer.appendChild(costumeContainerList);
