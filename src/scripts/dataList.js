6f840ae94d0539ef0effbfaa7558194c64223766
zxeni1 <zacxfitness@gmail.com>
2024-10-31 19:08:17 +1000

    Add feature

--- src/scripts/dataList.js
+++ src/scripts/dataList.js
@@ -15,12 +15,11 @@ let dataArray = [
 
 let costumesContainer = document.getElementById("costumeContainer");
 
-// Create an unordered list tag
-let costumesContainerList = document.createElement("ul");
-
 function renderData() {
+    // Create an unordered list tag
+    let costumesContainerList = document.createElement("ul");
     // Removes the existing content from the element
-    costumesContainerList.innerHTML = ""; // Use innerHTML to clear the list
+    costumesContainer.innerText = "";
 
     dataArray.forEach((costume) => {
         // Verify what we are working with
@@ -34,26 +33,45 @@ function renderData() {
     
         // Add a button to each entry that removes it from the list
         let removeButton = document.createElement("button");
+    
+        // Add a placeholder value/ name to the button
+        // removeButton.innerText = "Remove" + costume;
         removeButton.innerText = `Remove ${costume}`;
     
-        removeButton.onclick = () => {
-            removeCostumeFromDataList(costume);
-            renderData(); // Re-render the list after removal
-        };
-        
-        // Add the elements to the container
-        newCostumeEntry.appendChild(removeButton);
+        removeButton.onclick = (() => removeCostumeFromDataList(costume));
+        // Add the element to the container
         costumesContainerList.appendChild(newCostumeEntry);
+    
+        // Add the element to the container
+        newCostumeEntry.appendChild(removeButton);
+
     });
+    // Add the container to the HTML Page
+    costumesContainer.appendChild(costumesContainerList);
 }
 
-// Add the container to the HTML Page
-costumesContainer.appendChild(costumesContainerList);
 
-// Call renderData to initially populate the list
-renderData();
-
-function removeCostumeFromDataList(targetItemToRemove) {
-    dataArray = dataArray.filter((costume) => costume !== targetItemToRemove);
+function removeCostumeFromDataList (targetItemToRemove) {
+    dataArray = dataArray.filter((costume) => costume != targetItemToRemove);
+    renderData();
 }
 
+function addCostumeToDataList(event, targetInputId) {
+    // Prevent the form from doing its default behaviour (refreshing the page)
+    event.preventDefault();
+    console.log("Add costume to the list function is running!");
+
+    // Find the input text field based on targetInputId
+    let targetTextInput = document.getElementById(targetInputId);
+
+    // Get the value from the field
+    console.log(targetTextInput.value);
+    // Append or push to the dataArray
+    dataArray.push(targetTextInput.value);
+    // renderData function call to update the page
+    renderData();
+}
+
+// Find the form from the event
+let formInputButton = document.getElementById("costumesInputForm");
+formInputButton.addEventListener("click", (event) => addCostumeToDataList(event, "costumeInputText"));
