e99e8019b34e3ee367eb9bd926e1a3cbcb98d089
zxeni1 <zacxfitness@gmail.com>
2024-10-31 19:46:27 +1000

    quick fix for add button

--- src/README.md
+++ src/README.md
@@ -1,2 +1,10 @@
-# T3-W3-T
+# T3W3-Tuesday
 # JS DOM
+- Finding and reading elements
+- Creating and Deleting
+- Editing Elements
+
+# T3W3-Thursday
+- DOM Events
+- Forms
+- Netlify Deployment
--- src/scripts/dataList.js
+++ src/scripts/dataList.js
@@ -57,6 +57,18 @@ function removeCostumeFromDataList (targetItemToRemove) {
 }
 
 function addCostumeToDataList(event, targetInputId) {
+    // Find the form element that needs validation
+    let formElement = document.getElementById("costumesInputForm");
+
+    // Use the form element.checkValidity() method to save the result
+    let isFormValid = formElement.checkValidity();
+
+    // Do a conditional based on the result
+    if (!isFormValid){
+        formElement.reportValidity();
+        return;
+    }
+
     // Prevent the form from doing its default behaviour (refreshing the page)
     event.preventDefault();
     console.log("Add costume to the list function is running!");
@@ -68,10 +80,23 @@ function addCostumeToDataList(event, targetInputId) {
     console.log(targetTextInput.value);
     // Append or push to the dataArray
     dataArray.push(targetTextInput.value);
+
+    // Alert after submit - method 1
+    // alert("Submitted a new entry: " + targetTextInput.value);
+
+    // Clear out the input field text
+    targetTextInput.value = "";
+
+    // Focus on the text field to enable quick data entry
+    targetTextInput.focus();
+
+    // Alert after submit - method 2
+    alert("Submitted a new entry: " + dataArray[dataArray.length - 1]);
+
     // renderData function call to update the page
     renderData();
 }
 
 // Find the form from the event
-let formInputButton = document.getElementById("costumesInputForm");
+let formInputButton = document.getElementById("formInputButton");
 formInputButton.addEventListener("click", (event) => addCostumeToDataList(event, "costumeInputText"));
